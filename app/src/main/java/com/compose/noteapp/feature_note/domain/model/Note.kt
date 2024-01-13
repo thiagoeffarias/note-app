2a1cbaf73bdb6c32bf9363c68b9b7864aa6058c0
thiagoeffarias <thiagoferreirafarias@gmail.com>
2024-01-13 17:42:11 +0000

    Creates Note entity, dao and domain repository

---
+++ app/src/main/java/com/compose/noteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,24 @@
+package com.compose.noteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.compose.noteapp.ui.theme.BabyBlue
+import com.compose.noteapp.ui.theme.LightGreen
+import com.compose.noteapp.ui.theme.RedOrange
+import com.compose.noteapp.ui.theme.RedPink
+import com.compose.noteapp.ui.theme.Violet
+import java.sql.Timestamp
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Timestamp,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
